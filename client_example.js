5d4f2bb7a50852b564a1f878d3a6f946c117d0ab
Rafael A. Muñoz <ramunoz.19@est.ucab.edu.ve>
2022-11-06 15:19:12 -0400

    Codigo

--- client_example.js
+++ client_example.js
@@ -1,17 +1,17 @@
-const io = require('socket.io-client');
+import { io } from 'socket.io-client'
 
 let socket;
 
-socket = io.connect('https://guileless-muffin-fffeee.netlify.app/');
+socket = io.connect('http://localhost:3000');
 
 socket.emit("CONNECT", "topico", (response) => {
   console.log("CONNACK")
 });
 
-socket.emit("SUBSCRIBE", "/cocina/lucesAltas", (response) => {
+socket.emit("SUBSCRIBE", "home/cocina/lucesAltas", (response) => {
   console.log(response);
 });
 
-socket.emit("PUBLISH", "/cocina/termometro", "79", (response) => {
+socket.emit("PUBLISH", "home/kitchen/room_lights", "79", (response) => {
   console.log(response);
 })
--- index.js
+++ index.js
@@ -8,88 +8,118 @@ let server = app.listen(3000);
 
 app.use(express.static('public'));
 
+/* Arreglo de topicos principales 
+    ej. Home/
+        Edif/
+*/
+
 let topics = [];
 
-const io = new Server(server)
+const io = new Server(server);
 
+function getTopics(complete_topic){
+    
+    let topics = complete_topic.split('/');
+
+    return topics;
+}
+
+//clase topico.
+
+class topic {
+
+    // Este constructor se aplica a las diferentes aplicaciones a las cuales se deban aplicar.
+
+    constructor(topic_name){
+        this.topic_name = topic_name;
+        this.suscribers = [];
+        this.subTopic = [];
+        this.SavedMessage;
+        this.lastWillMessage = "service disconnected.";
+    }
+
+    addSubscriber(ID_client){
+        this.suscribers.push(ID_client);
+    }
+
+    setSavedMessage(SavedMessage){
+        if(this.suscribers.lenght == 0){
+            this.SavedMessage = SavedMessage;
+        }
+    }
+
+    writeLastWillMessage(){
+        return(lastWillMessage);
+    }
+
+    //funcion que crea sub topicos.
+
+    createSubTopics(arrayOfTopics, ID_client, message, mode){
+
+        if(arrayOfTopics.length == 0 && mode == 0){
+
+            this.suscribers.push(ID_client);
+            this.SavedMessage = message;
+            return;
+        }
+
+        if(arrayOfTopics.length == 0){
+            return;
+        }
+
+        let newTopic = new topic(arrayOfTopics[0]);
+
+        this.subTopic.push(newTopic);
+
+        for (let i = 0; i < this.subTopic.length; i++) {
+            
+            if(this.subTopic[i].topic_name == arrayOfTopics[0]){
+                arrayOfTopics.shift();
+                this.subTopic[i].createSubTopics(arrayOfTopics, ID_client, message, mode);
+            }
+        }
+        
+    }
+}
+
+// Comienza el servidor y ejecuta una funcion cuando haya una nueva conexión.
 io.sockets.on('connection', newConnection);
 
-function AddTopic(topic, message){
-    console.log("agregue un topico");
-    topics.push( io.of(topic) );
-}
-
-function subscribeToTopic(topic, socket){
-
-    topics.forEach(element => {
-        if(topic == element.name){
-            console.log("entre en subscribeToTopic");
-            
-            element.on("connection", () => {
-                console.log("subscrito");
-            })
-        }
-    })
-}
 
 function newConnection(socket){
 
+    // Escucha a la llamada Connect.
+
     socket.on("CONNECT", (arg, callback) => {
-        console.log(arg);
         callback("CONNACK");
     });
     
-    socket.on("PUBLISH", (topic, message, callback) => {
-        console.log(topic);
+    //Escucha a la llamada PUBLISH del cliente. 
+    // Recibe el directorio o topico, ej. Home/ ; Home/kitchen/termometer.
+    // message es la informacion que el cliente quiere publicar, ej. 79 grados, prender, apagar
+    // Callback es la respueta que da el broker con respecto a la peticion del cliente.
+    socket.on("PUBLISH", (topic_dir, message, callback) => {
 
-        if(topics.length == 0){
-            AddTopic(topic, message);
-            console.log("agregue un nuevo topic");
-        } else {
-            topics.forEach(element => {
+        let arrayOfTopics = getTopics(topic_dir);
+
+        let newTopic = new topic(arrayOfTopics[0]);
+
+        topics.push(newTopic);
+
+        for (let i = 0; i < topics.length; i++) {
             
-                if(topic == element){
-                    console.log("Entre acá");
-                } else {
-                    console.log("Entre en el else");
-                    AddTopic(topic, message);
-                }
-            });
+            if(topics[0].topic_name == arrayOfTopics[0]){
+                arrayOfTopics.shift();
+                topics[i].createSubTopics(arrayOfTopics, socket.ID, message, 0);
+            }
         }
         
-        topics.forEach(element => {
-            console.log("entre");
-            if(topic == element.name){
-                console.log("entre");
-                console.log(element.sockets.size);
-            }
-        })
-
-        callback("PUBACK");
+        console.log(topics[0].subTopic[0].subTopic[0].suscribers);
+        console.log(topics[0].subTopic[0].subTopic[0].SavedMessage);
     });
 
     socket.on("SUBSCRIBE", (topic, callback) => {
-        
-        if(topics.length == 0){
 
-            AddTopic(topic, null);
-            subscribeToTopic(topic, socket);
-            
-            console.log(topics.length);
-
-            topics.forEach( element => {
-
-                console.log("Im here");
-
-                if(element.name == topic ){
-                    console.log("entre aqui mrc")
-                }
-
-                console.log(element.sockets.size);
-            })
-        } else {
-
-        }
         callback("SUBACK");
     });
 
