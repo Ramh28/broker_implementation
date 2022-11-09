3137ff6c99c0330048729ba9c7e8b7d0c2e23d71
Wladimir <wladimirss14@gmail.com>
2022-11-08 23:06:37 -0400

    funcion suscribir funciona pero puede mejorar la legibilidad, hay comentarios que ayudan a entender el codigo

--- client_example.js
+++ client_example.js
@@ -2,22 +2,24 @@ const io  = require('socket.io-client')
 
 let socket;
 let msg = 'saludo desde mi sofa';
-let ruta = 'home/room/tv/'
+let ruta = '/home/room/tv/'
 socket = io.connect('http://localhost:3000');
 
 socket.on("connect", () => {
   console.log('cliente conectado: ' + socket.connected); // true
 });
 
-socket.emit("PUBLISH", msg, ruta, (response) => {
-  console.log(response); // "got it"
-});
+// socket.emit("PUBLISH", msg, ruta, (response) => {
+//   console.log(response); // "got it"
+// });
+
 socket.emit("SUBSCRIBE", msg, ruta, (response) => {
   console.log(response); // "got it"
 });
-socket.emit("UNSUBSCRIBE", msg, ruta, (response) => {
-  console.log(response); // "got it"
-});
+
+// socket.emit("UNSUBSCRIBE", msg, ruta, (response) => {
+//   console.log(response); // "got it"
+// });
 
 
 
--- index.js
+++ index.js
@@ -80,8 +80,6 @@ class Topic{
     }
 }
 
-let topic = new Topic('/');
-
 function escribirLog(socket, ruta, evento){
     fs.appendFile('./logs.txt', 'autor: ' + socket.conn.remoteAddress + ' Topico: '+ ruta + ' evento: ' + evento + '\n', (error)=>{
         if (error){
@@ -92,85 +90,105 @@ function escribirLog(socket, ruta, evento){
 
 function suscribe(topicActual,route,idCliente) {
 
+    console.log(topicActual.topicName + " estoy adentro ");
+
     let indexSlash = route.indexOf('/');
-    let rutaActual = route.slice(1, indexSlash+1);
+    // let siguiente = route.slice(indexSlash+1, route.length);
 
-    // Se alcanzo la ruta maxima.
+    // console.log('el siguiente es: ' + siguiente);
 
+    // console.log(indexSlash);
+    // console.log('soy el length '+route.length);
+    // console.log(route);
+    // console.log(route.slice(0, indexSlash+1));
+
+    // Si estamos en raiz y la ruta marca la raiz
     if ( indexSlash == 0 && route.length == 1 ){
 
         topicActual.addSubscriber(idCliente);
-
+        console.log(" guardare en raiz " + topicActual.topicName);
         return;
     }
 
-    if ( route.length == 0 ) {
+    // si estamos en Raiz y la ruta continua
+    if (indexSlash == 0 && route.length > 0){
+        console.log(" paso la raix raiz " + topicActual.topicName);
+        console.log(route.slice(indexSlash+1, route.length))
+
+        topicActual.subTopic.forEach(element => {
+            console.log('entre en foreach')
+            if( element.topicName == route.slice(0, indexSlash+1) ){
+                console.log('entre en foreach condicional')
+
+                suscribe(element, route.slice(indexSlash+1, route.length), idCliente)
+                return;
+            }
+        });
+
+        // si no consegui el topico que quiero entonces creo uno de ultimo y continuo
+        topicActual.addSubTopic( route.slice(indexSlash+1, route.length) );
+        console.log('cree el subtopico nuevo: '+ topicActual.subTopic[topicActual.subTopic.length-1].topicName)
+
+        suscribe( topicActual.subTopic[topicActual.subTopic.length-1], route.slice(indexSlash+1, route.length), idCliente );
+        return;
+    }
+
+    // si alcanzamos fin de ruta
+    if ( route.slice(indexSlash+1, route.length).length == 0 ) {
         topicActual.addSubscriber(idCliente);
 
+        console.log(" guardare en un topico " + topicActual.topicName);
         return;
     }
 
+    // si nos quedamos sin topicos y la ruta sigue
     if ( indexSlash == -1 && route.length > 0 ){
-        
-        let continuacionRuta = route.slice(indexSlash+1, route.length);
-        
-        topicActual.addSubTopic(rutaActual);
 
-        suscribe( topicActual.subTopic[0], continuacionRuta, idCliente );
+        topicActual.addSubTopic(route.slice(0, indexSlash+1));
+        console.log('cree el subtopico nuevo: '+ topicActual.subTopic[0].topicName)
+        suscribe( topicActual.subTopic[0], route.slice(indexSlash+1, route.length), idCliente );
 
         return;
     }
 
+    // Si el slash es mayor a 0 y la ruta tambien
+    if ( indexSlash > 0 && route.length > 0 ){
+        console.log(indexSlash);
+        console.log(" Me muevo de directorio " + topicActual.topicName);
 
+        // si no tengo mas topicos creo uno y continuo
+        if (topicActual.subTopic.length == 0){
+            topicActual.addSubTopic(route.slice(indexSlash+1, route.length));
+            console.log('cree el subtopico nuevo: '+ topicActual.subTopic[0].topicName)
 
+            suscribe( topicActual.subTopic[0], route.slice(indexSlash+1, route.length), idCliente );
+            return;
+        }
+    
+        // si tengo topicos los reviso uno a uno hasta conseguir el que es
+        topicActual.subTopic.forEach(element => {
+            console.log('entre en foreach')
+            if( element.topicName == route.slice(0, indexSlash+1) ){
+                console.log('entre en foreach condicional')
 
+                suscribe(element, route.slice(indexSlash+1, route.length), idCliente)
+                return;
+            }
+        });
 
-    // console.log(actualRaiz = route.slice(0, posicionRaiz+1));
-    // console.log(siguienteRaiz = route.slice(posicionRaiz+1))
-    // if (posicionRaiz!=0) {
-    //     actualRaiz = route.slice(0, posicionRaiz+1);
-    //     siguienteRaiz = route.slice(posicionRaiz+1);
+        // si no consegui el topico que quiero entonces creo uno de ultimo y continuo
+        topicActual.addSubTopic( route.slice(indexSlash+1, route.length) );
+        console.log('cree el subtopico nuevo: '+ topicActual.subTopic[topicActual.subTopic.length-1].topicName)
 
-    //     if (siguienteRaiz.length == 0) {
-    //         topicActual.addSubscriber(idCliente);
-    //     }else{
+        suscribe( topicActual.subTopic[topicActual.subTopic.length-1], route.slice(indexSlash+1, route.length), idCliente );
+        return;
 
-    //         let indice = topicActual.subTopic.indexOf(actualRaiz);
-            
-    //         if (indice== -1) {
-    //             topicActual.addSubTopic(actualRaiz);
-    //             indice = topicActual.subTopic.indexOf(actualRaiz);
-    //         }
-    //         // console.log('suscrito')
-    //         suscribe(topicActual.subTopic[indice],siguienteRaiz,idCliente);
-    //     }
-    // }else{
-    //     // console.log('suscritor annadido');
-    //     // console.log(topicActual.topicName);
-    //     topicActual.addSubscriber(idCliente);
-    // }
+    }
 };
 
-// Comienza el servidor y ejecuta una funcion cuando haya una nueva conexión.
-// io.sockets.on('connection', newConnection);
-
+let topic = new Topic('/');
 
 io.on('connect', (socket) => {
-    console.log('conecte: ' + socket.id);
-    console.log(topic.topicName);
-  
-    escribirLog(socket, '/', 'connect')
-
-    // socket.on('PUBLISH', (msg, route) => {
-    //     topic.emitPublish(msg, route, topic);
-    // });
-    
-    // socket.on('SUBSCRIBE', (route) => {
-    //     suscribe(topic,route,socket.id);
-    // });
-    // socket.on('UNSUBSCRIBE', () => {
-    //     unsuscribe(topic,route,socket.id)
-    // });
 
     socket.on('PUBLISH', (msg, ruta, callback) => {
         // topic.emitPublish(msg, route, topic);
@@ -181,11 +199,8 @@ io.on('connect', (socket) => {
     });
 
     socket.on('SUBSCRIBE', (msg, ruta, callback) => {
-        // suscribe(topic,route,socket.id);
 
         suscribe(topic,ruta,socket.id);
-
-        console.log('suscritor de ' + topic.topicName)
 
         escribirLog(socket, ruta, 'SUBSCRIBE');
         callback("SUBSCRIBE CON EXITO");
@@ -197,9 +212,9 @@ io.on('connect', (socket) => {
         escribirLog(socket, ruta, 'UNSUBSCRIBE')
         callback("UNSUBSCRIBE CON EXITO");
     });
-    
+
 });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
-  });
+});
